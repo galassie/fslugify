f280bbada07da3be8a6e0367136767199be70eff
Enrico Galassi <enrico.galassi.88@gmail.com>
2019-10-05 17:10:49 +0200

    Add space-between-words case

--- src/SlugGenerator.fs
+++ src/SlugGenerator.fs
@@ -5,4 +5,10 @@ module SlugGenerator =
     type SlugGeneratorOptions = string array
 
     let slugify (options: string array) (toSlugify: string) =
+        let mapChar char = 
+            match char with
+            | ' ' -> "-"
+            | _ -> sprintf "%c" char
+
         toSlugify
+        |> String.collect mapChar
--- tests/SlugGeneratorTests.fs
+++ tests/SlugGeneratorTests.fs
@@ -6,6 +6,7 @@ open FSharp.Slugify.SlugGenerator
 type SlugGeneratorTests() =
 
     [<TestCase("test", "test")>]
+    [<TestCase("url with spaces", "url-with-spaces")>]
     member this.``Test slugify method`` (input, expectedOutput) =
         let stringSlugified = slugify [||] input
-        Assert.AreEqual(stringSlugified, expectedOutput)
+        Assert.AreEqual(expectedOutput, stringSlugified)
